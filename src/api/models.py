c0553ad8d79f10c04771a514b0eeafd357b3cb5b
mistledi <bkmtova.diana@gmail.com>
2023-05-30 20:12:15 +0600

    fix: User model

--- src/api/models.py
+++ src/api/models.py
@@ -1,23 +1,27 @@
-from passlib.apps import custom_app_context as pwd_context
+from datetime import datetime
+import bcrypt
 from api import db
+
 
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    is_admin = db.Column(db.Boolean()),
-    last_login = db.Column(db.DateTime())
-    created_at = db.Column(db.DateTime())
-    updated_at = db.Column(db.DateTime())
+    password = db.Column(db.String(150), nullable=False)
+    is_admin = db.Column(db.Boolean(), default=False)
+    last_login = db.Column(db.DateTime(), default=datetime.now())
+    created_at = db.Column(db.DateTime(), default=datetime.now())
+    updated_at = db.Column(db.DateTime(), default=datetime.now())
 
-    def hash_password(self, password) -> None:
-        self.password = pwd_context.encrypt(password)
-        
-    def verify_password(self, password) -> bool:
-        return pwd_context.verify(password, self.hashed_password)
+
+    def hash_password(self, plain_text_password):
+        password_bytes = plain_text_password.encode('utf-8')
+        self.password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+
+    def verify_password(self, plain_text_password):
+        return bcrypt.checkpw(plain_text_password, self.password)
 
     def __repr__(self):
         return f'<User {self.username}>'

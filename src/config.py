b054d2d8e375b628a8785f1656f82d87b116588e
mistledi <bkmtova.diana@gmail.com>
2023-05-30 19:00:51 +0600

    feat: add EMAIL and Token configuration

--- src/config.py
+++ src/config.py
@@ -12,3 +12,18 @@ class RedisConfig:
     REDIS_PORT = int(environ.get('REDIS_PORT'))
     REDIS_DB = int(environ.get('REDIS_DB'))
     REDIS_EXPIRATION = int(environ.get('REDIS_EXPIRATION'))
+
+
+class TokenConfig:
+    SECRET_KEY = environ.get('SECRET_KEY')
+    SECURITY_PASSWORD_SALT = environ.get('SECURITY_PASSWORD_SALT')
+    
+
+class EmailConfig:
+    MAIL_SERVER = environ.get('MAIL_SERVER')
+    MAIL_PORT = int(environ.get('REDIS_PORT'))
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = environ.get('EMAIL')
+    MAIL_PASSWORD = environ.get('PASSWORD')
+    MAIL_DEFAULT_SENDER = environ.get('MAIL_DEFAULT_SENDER')

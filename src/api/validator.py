addc3f1612e19297daad896c7551d0bd70bdb9c9
mistledi <bkmtova.diana@gmail.com>
2023-06-05 15:43:22 +0600

    fix: Updated password length message in is_valid_password function

--- src/api/validator.py
+++ src/api/validator.py
@@ -2,12 +2,11 @@ import re
 
 
 def is_valid_password(password: str) -> bool | str:
-
     MIN_LENGTH: int = 8
     MAX_LENGTH: int = 64
 
     if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
-        return "Password must at least 8 characters long"
+        return "Password must be between 8 and 64 characters long"
 
     if not re.search(r'[a-z]', password):
         return "Password must contain at least one lowercase letter"
@@ -19,7 +18,6 @@ def is_valid_password(password: str) -> bool | str:
 
 
 def is_valid_email(email: str) -> bool:
-
     pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
 
     if re.match(pattern, email):
